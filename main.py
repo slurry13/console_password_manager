43a21b42dc54a49594052da07dc0f403f6106e11
slurry <matvei0972@gmail.com>
2026-09-26 13:26:46 +0300

    fix(main):handshake with db

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import database
 import crypto
 import session
 import getpass
+from cryptography.fernet import Fernet
 
 database.init_db()
 
-ch_phrase = "hi_i_am_here"
+CHECK_PHRASE = "hi_i_am_here"
 
 def registration():
     new_username = input("Username: ")
@@ -14,8 +15,10 @@ def registration():
     u_salt = crypto.make_salt()
     fer_us = crypto.fernet(new_password, u_salt)
 
-    enc_phrase = fer_us.encrypt(ch_phrase.encode('utf-8'))
+    enc_phrase = fer_us.encrypt(CHECK_PHRASE.encode('utf-8'))
     a_verif = enc_phrase.decode('utf-8')
+
+    print(a_verif)
 
     database.new_user(new_username, u_salt, a_verif)
 
@@ -23,15 +26,24 @@ def login():
     username = input("Username: ")
     password = getpass.getpass()
 
-    salt, a_verif = database.get_val(username)
+    salt, a_db_verif, id_us = database.get_val(username)
 
-    fer_check = crypto.fernet(password, salt)
+    try:
+        fer_user = crypto.fernet(password, salt)
+        decrypted_phrase = fer_user.decrypt(a_db_verif.encode('utf-8')).decode('utf-8')
 
-    enc_phrase = fer_check.encrypt(ch_phrase.encode('utf-8'))
-    verif_ph = enc_phrase.decode('utf-8')
+        if decrypted_phrase == CHECK_PHRASE:
+            print("Scs: Enter")
+            session.Session.user_id = id_us
+            session.Session.fernet_user = username
+            session.Session.is_active = True
 
-    if a_verif == verif_ph:
-        print("scs")
+        else:
+            print("Err: Anexpected master pass")
+
+    except Exception:
+        print("Err: Anexpected master pass")
+
 
 while(True):
     cmd = input()
